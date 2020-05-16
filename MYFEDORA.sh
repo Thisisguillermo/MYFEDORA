8f652844ecc5a6bc3cbc77707e711a6672a879ed
Guillermo <guillermozaandam@hotmail.com>
2020-05-16 17:05:12 +0200

    gnome-firmware

--- MYFEDORA.sh
+++ MYFEDORA.sh
@@ -43,6 +43,7 @@ sudo dnf install -y jpegoptim
 sudo dnf install -y ffmpeg 
 sudo dnf install -y gnome-tweaks 
 sudo dnf install -y gnome-documents
+sudo dnf install -y gnome-firmware
 sudo dnf install -y toolbox 
 sudo dnf install -y deja-dup 
 sudo dnf install -y chrome-gnome-shell 
